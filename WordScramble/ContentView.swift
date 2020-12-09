7e91d9d8cd71a0cda726d1aba3c9c922e7f90d26
Rick Brown <ricky.brown.00@gmail.com>
2020-12-09 11:53:57 +0000

    adds words to a list

--- WordScramble/ContentView.swift
+++ WordScramble/ContentView.swift
@@ -8,14 +8,43 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        Text("Hello, world!")
-            .padding()
+  @State private var usedWords = [String]()
+  @State private var rootWord = ""
+  @State private var newWord = ""
+  
+  var body: some View {
+    NavigationView {
+      VStack {
+        TextField("Enter your word", text: $newWord, onCommit: addNewWord)
+          .textFieldStyle(RoundedBorderTextFieldStyle())
+          .autocapitalization(.none)
+          .padding()
+        
+        List(usedWords, id: \.self) {
+          Image(systemName: "\($0.count).circle")
+          Text($0)
+        }
+      }
+      .navigationBarTitle(rootWord)
     }
+  }
+  
+  func addNewWord() {
+    /// Format and store the new word as a variable.
+    let answer = newWord.lowercased().trimmingCharacters(in: .whitespacesAndNewlines)
+    /// Handle the case where new word is an empty string.
+    guard answer.count > 0 else {
+      return
+    }
+    /// Add the formatted new word to the usedWords array.
+    usedWords.insert(answer, at: 0)
+    /// Reset the new word.
+    newWord = ""
+  }
 }
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+  static var previews: some View {
+    ContentView()
+  }
 }
